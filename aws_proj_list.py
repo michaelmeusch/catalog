9f85d60020c856d19422181493bde006c3e87903
Michael Meusch <michaelmeusch@gmail.com>
2020-06-17 22:26:37 -0400

    new projet file

---
+++ aws_proj_list.py
@@ -0,0 +1,64 @@
+
+def lambda_handler(context,event):
+
+
+    AWS_services = [
+    ' S3'
+    ' EC2 ',
+    ' Lambda',
+    ' Glacier'
+    ' SNS',
+    ' CloudFront',
+    ' EBS',
+    ' Kinesis'
+    ]
+
+    #Create a list of awsv services
+    # these are the list that show specific servies .
+    value1 = AWS_services
+    value2 = AWS_services
+    value_1 = ord ('A-G')
+    Value_2 = ord('H-O')
+    Value_3 = ord ('P-Z')
+    # item = [0]
+    print(AWS_services)
+    for AWS_services in a_list:
+        if ord(value_1[0])>= ord(Value_2[7])
+    derived_list_1.append(item)
+
+        print(item,"")
+    # a_list = sorted(AWS_services)
+    # print(a_list)
+    # item = 0
+    # item = '#'.join (AWS_services)
+    # # print(item)
+    # for item in a_list:
+    #     print(item,"")
+
+
+        # Near_extinc = []
+
+        # if  item in AWS_services:
+        #     print=
+        # elif item == :
+
+
+            ##Create three new derived lists for input list names that start with: A-G, H-O & P-Z
+            # first_letters = ['A,B,C,D,E,F,G']
+            # output_names = [AWS_services for AWS_services in AWS_services if (AWS_services[0] in first_letters)]
+            # output_names
+            # [' S3.'
+            # ' EC2 ',
+            # ' Lambda.',
+            # ' Glacier.'
+            # ' SNS.',
+            # ' CloudFront.',
+            # ' EBS',
+            # ' Kinesis.']
+
+            # print()
+
+            # output_names.sort()
+            # output_names.append(item) if item not in AWS_services else None
+            # print(item)
+
